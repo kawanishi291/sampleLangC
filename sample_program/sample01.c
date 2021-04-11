625ae33ec1ee29b19e888c72ab4f41027862bd6d
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-04-11 21:09:51 +0900

    sample01.c編集

--- sample_program/sample01.c
+++ sample_program/sample01.c
@@ -11,15 +11,20 @@ int main(void) {
     }
 
     int max = 0;
+    int min = 0;
     int sum = 0;
 
     for (int j = 0; j < 10; j++){
         if (max < array[j]){
             max = array[j];
         }
+        if (min > array[j]){
+            min = array[j];
+        }
         sum = sum + array[j];
     }
     printf("最大値は%d\n", max);
+    printf("最小値は%d\n", min);
     printf("合計値は%d\n", sum);
 
     return 0;
