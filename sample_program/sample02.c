82f0fced377f2db5f6d5b50b127604f9f344cb61
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-04-12 18:57:12 +0900

    sample02.c

---
+++ sample_program/sample02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// Š|‚¯ŽZ
+
+int main(void) {
+	int i;
+	int j;
+
+    for (j = 1; j < 10; j++){
+    	for (i = 1; i < 10; i++){
+        	printf("%d\t", i*j);
+    	}
+    	printf("\n");
+    }
+
+    return 0;
+}
