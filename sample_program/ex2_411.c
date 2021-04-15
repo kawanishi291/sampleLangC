b4b9d243cd6bd1edaa3cfb8f5806492015376cc9
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-04-15 17:55:45 +0900

    for文

--- sample_program/ex2_411.c
+++ sample_program/ex2_411.c
@@ -1,28 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
 
 int main(void) {
-    int num;
-    int cnt, cnt2;
-    int i, j;
+    char str[100+1];
+    int len;
+    int i;
 
-    printf("整数を入力してください => \n");
-    scanf("%d", &num);
-    printf("%d以下の素数：", num);
-    cnt2 = 0;
-    for(j = 2; j <= num; j++){
-        cnt = 0;
-        for(i = 2; i <= j; i++){
-            if(j % i == 0){
-                cnt += 1;
-            }
-        }
-        if(cnt < 2){
-            printf("%d, ", j);
-            cnt2 += 1;
-        }
+    printf("文字列(100字まで)を入力してください => \n");
+    scanf("%s", str);
+    len = strlen(str);
+    for(i = 1; i <= len; i++){
+        printf("%c", str[len - i]);
     }
-    printf("\n%d個の素数があります\n", cnt2);
+    printf("\n");
 
     return 0;
 }
