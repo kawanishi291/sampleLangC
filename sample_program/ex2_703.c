295abbf7582d2776a3b8055d229d781c3a38da1f
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-04-16 22:53:15 +0900

    編集

--- sample_program/ex2_703.c
+++ sample_program/ex2_703.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 struct S_POINT {
     int x;
@@ -6,14 +7,24 @@ struct S_POINT {
 };
 
 int main(void) {
-    struct S_POINT point = {
-        200,
-        150
+    int dx;
+    int dy;
+    struct S_POINT point1 = {
+        100,
+        80
     };
-    struct S_POINT *p = &point;
+    struct S_POINT point2 = {
+        320,
+        250
+    };
+    struct S_POINT *p1 = &point1;
+    struct S_POINT *p2 = &point2;
+    dx = p1->x - p2->x;
+    dx *= dx;
+    dy = p1->y - p2->y;
+    dy *= dy;
 
-    printf("x座標：%d\n", p->x);
-    printf("y座標：%d\n", p->y);
+    printf("点(%d:%d)と、点(%d:%d)の距離は、%.2fです\n", p1->x, p1->y, p2->x, p2->y, sqrt(dx + dy));
 
     return 0;
 }
--- sample_program/ex2_704.c
+++ sample_program/ex2_704.c
@@ -8,22 +8,25 @@ struct S_WIZARD {
 };
 
 // 関数プロトタイプ宣言
-void printData(struct S_WIZARD *);
+void setData(struct S_WIZARD *);
 
 int main(void) {
-    struct S_WIZARD wizard = {
-        501,
-        "Ellie",
-        2000
-    };
+    struct S_WIZARD wizard;
+    struct S_WIZARD *p = &wizard;
 
-    printData(&wizard);
-
-    return 0;
-}
-
-void printData(struct S_WIZARD *p){
+    setData(p);
+    printf("\nデータは構造体に保存されました!\n\n");
     printf("ID　：%d\n", p->id);
     printf("名称：%s\n", p->name);
     printf("魔力：%d\n", p->magic);
+    return 0;
+}
+
+void setData(struct S_WIZARD *p){
+    printf("ID => \n");
+    scanf("%d", &p->id);
+    printf("名称 => \n");
+    scanf("%s", p->name);
+    printf("魔力 => \n");
+    scanf("%d", &p->magic);
 }
