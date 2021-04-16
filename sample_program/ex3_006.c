651929e86abe0a4da5b70187e20fd3f6e73a341d
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-04-17 02:19:45 +0900

    フロア案内

---
+++ sample_program/ex3_006.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+    int floor;
+
+    floor = 1;
+    while(1){
+        printf("何階に行きますか？(1〜3)\n");
+        scanf("%d", &floor);
+        
+        if(floor == 1){
+            printf("%d階：化粧品売り場\n", floor);
+        }else if(floor == 2){
+            printf("%d階：紳士服売り場\n", floor);
+        }
+        else if(floor == 3){
+            printf("%d階：婦人服売り場\n", floor);
+        }else if(floor == 0 || floor == 9){
+            break;
+        }else{
+            printf("その階には行けません\n");
+        }
+    }
+}
