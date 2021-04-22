eb229f0517a04687f2c28e93d6fbcb91ebd6d3ff
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-04-22 19:23:47 +0900

    getFunc関数

--- ComprehensiveSeminar/getFunc.c
+++ ComprehensiveSeminar/getFunc.c
@@ -5,23 +5,30 @@
 int getString(char *prompt, char *buffer, int size)
 {
     int len;
-    int flag;
+    int flag = -1;
+	
+	if (size >= 2 && buffer != NULL){ 
+		if (prompt == NULL) {
+			printf("=> ");
+		} else {
+			printf("%s", prompt);
+		}
+	    while(flag == -1){
+	        fgets(buffer, size, stdin);
+	        len = strlen(buffer);
+	        if (buffer[len - 1] == '\n') {
+	            buffer[len - 1] = '\0';
+	            flag = 0; //正常系の値0を代入し、ループを抜ける
+	        } else {
+	        	// エラーメッセージの代入
+	        	prompt = "<98文字以内で入力してください>\ninput => ";
+	            printf("%s", prompt);
+	        	// バッファをクリア
+	            while (getchar () != '\n') {
+	            }
+	        }
+	    }
+	}
 
-    flag = 0;
-    while(flag == 0){
-        printf("文字を入力してください(98文字以内)\n%s\n", prompt);
-        fgets(buffer, size, stdin);
-        len = strlen(buffer);
-        if (buffer[len - 1] == '\n') {
-            buffer[len - 1] = '\0';
-            printf("%d文字\n",len);
-            flag = 1;
-        } else {
-            printf("98オーバー!!%d文字\n", len);
-            while (getchar () != '\n') {
-            }
-        }
-    }
-
-    return 0;
+    return flag;
 }
