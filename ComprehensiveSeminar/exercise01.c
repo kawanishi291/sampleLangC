ec53b3c3e552344cdc1d4e1029da34c5a225e9e4
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-04-22 19:23:08 +0900

    exercise01.c（main関数）

--- ComprehensiveSeminar/exercise01.c
+++ ComprehensiveSeminar/exercise01.c
@@ -3,22 +3,38 @@
 #include "getFunc.h"
 
 int main(void){
-    char promptString[] = "input => ";
+    char promptString[] = "文字を入力してください(98文字以内)\ninput => ";
     char inputArray[100];
     int i, j;
     int len;
+	int partitions;
     int result;
 
+	// 関数呼び出し
     result = getString(promptString, inputArray, 100);
+	// 入力文字列の要素数格納
     len = strlen(inputArray);
-    for (i = 0; i < len / 10 + 1; i++) {
-        printf("分割(%d)：", i);
-        for (j = 0; j < 10; j++){
-            printf("%c", inputArray[j+i*10]);
+	// 分割回数を求める
+	partitions = len / 10 + 1;
+	if (len % 10 == 0) {
+		partitions -= 1; // 割り切れた場合は 1足さない
+	}
+	printf("--------------------\n");
+    for (i = 0; i < partitions; i++) {
+        printf("分割(%d)：", i + 1);
+        for (j = 0; j < 10; j++) {
+        	// 入力文字数を超えてないかチェック
+        	if (j + i * 10 <= len) {
+        		// 分割数(外ループ)に10を掛けて、内ループ回数を足す
+        		printf("%c", inputArray[j+i*10]);
+        	} else {
+        		break; // 超えた場合はループを抜ける
+        	}
         }
         printf("\n");
     }
-    printf("%d", len);
+	printf("--------------------\n");
+    printf("%d文字表示しました", len);
 
-    return 0;
+    return result;
 }
