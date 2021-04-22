489e75f573225f9972027613152151490c60f751
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-04-22 22:46:01 +0900

    exercise02.c（main関数）

--- ComprehensiveSeminar/exercise02.c
+++ ComprehensiveSeminar/exercise02.c
@@ -8,6 +8,8 @@ int main(void){
 	int ary[11];
 	int cnt = 0;
 	int result;
+	int i;
+	int size;
 	int num;
 	
 	printf("0～10までの数値を10回入力してください\n");
@@ -15,15 +17,37 @@ int main(void){
 	while (cnt < 10) {
 		result = getNumber(promptMessage, 0, 10);
 		// 戻り値が -1 はエラー, その他は入力値
-		if (result != -1) {
+		if (0 <= result && result <= 10) {
 			ary[cnt] = result;
 			cnt += 1;
+		} else if(result == -1) {
+			ary[cnt] = result;
+			cnt += 10;
 		}
 	}
-	num = getOddMax(ary, sizeof ary / sizeof ary[0] - 1);
-	printf("\n奇数で一番大きな数値：%d\n", num);
-	num = getSecondMin(ary, sizeof ary / sizeof ary[0] -1);
-	printf("小さい方から2番目の数値：%d\n", num);
+	// 入力された数値の個数(size)を求める
+	for (i = 0; i < 10; i++) {
+		if (ary[i] == -1) {
+			size = i;
+			break;
+		} else {
+			size = 10;
+		}
+	}
+	// 奇数で一番大きな数値を見つける処理(関数を呼ぶ)
+	num = getOddMax(ary, size);
+	if (num == -1){
+		printf("奇数で一番大きな数値：なし\n");
+	} else {
+		printf("奇数で一番大きな数値：%d\n", num);
+	}
+	// 小さい方から2番目の数値を見つける処理(関数を呼ぶ)
+	num = getSecondMin(ary, size);
+	if (num == -1) {
+		printf("小さい方から2番目の数値：なし\n");
+	} else {
+		printf("小さい方から2番目の数値：%d\n", num);
+	}
 	
     return 0;
 }
