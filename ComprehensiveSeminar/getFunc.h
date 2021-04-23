c9133967fa6e76264b0bd047cfaa79bd595e2ee4
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-04-23 18:59:12 +0900

    ヘッダファイル

--- ComprehensiveSeminar/getFunc.h
+++ ComprehensiveSeminar/getFunc.h
@@ -2,3 +2,5 @@
 int getString(char *prompt, char *buffer, int size);
 // 総合演習２
 int getNumber(char *prompt, int minValue, int maxValur);
+// 総合演習４
+int getDayNumber(char *prompt, int minValue, int maxValue);
