cd840a2c6df4b1ef6a1b154fdc5f1b7b70d14708
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-04-26 19:25:55 +0900

    exercise05.c (main関数)

--- ComprehensiveSeminar/exercise05.c
+++ ComprehensiveSeminar/exercise05.c
@@ -12,46 +12,124 @@ typedef struct {
 } ST_TEST;
 
 int inputData(ST_TEST *pTest);
+void sortPoint(ST_TEST *pTest);
+void sortName(ST_TEST *pTest);
+void printData(ST_TEST *pTest);
 
 
 int main(void){
-    int result;
+	int result;
     int i;
     ST_TEST students[STUDENT_SIZE];
+	int type = 1;
 
     result = inputData(students);
-    printf("name\tpoint\n");
-    for(i = 0; i < 22; i++){
-        printf("-");
-    }
-    printf("\n");
-    for (i = 0; i < 5; i++) {
-        printf("%s\t%d\n", students[i].name, students[i].point);
-    }
+	printData(students);
+	while (1) {
+		printf("1:点数順/ 2:名前順/ 0:終了 => \n");
+		scanf("%d", &type);
+		if (type == 1) {
+			sortPoint(students);
+			printData(students);
+		} else if(type == 2) {
+			sortName(students);
+			printData(students);
+		} else if (type == 0) {
+			break;
+		}
+	}
     
     return 0;
 }
 
+
+/* データ入力関数 */
 int inputData(ST_TEST *pTest){
     char promptMessage[] = "<5件分のデータを入力してください>\nname => \n";
     char inputArray[100];
     int cnt = 0;
-    int result;
-    int point;
+    int result = -1;
+    int point = -1;
 
-    while (cnt < 5) {
-        result = getString(promptMessage, inputArray, 100);
-        if (result >= 0) {
-            strcpy(pTest[cnt].name, inputArray);
-            strcpy(promptMessage, "point => \n");
-        }
-        point = getNumber(promptMessage, 0, 100);
-        if (point >= 0) {
-            pTest[cnt].point = point;
-            strcpy(promptMessage, "name => \n");
-        }
+    while (cnt < STUDENT_SIZE) {
+    	while (result < 0) {
+    		result = getString(promptMessage, inputArray, 100);
+	        if (result >= 0) {
+	            strcpy(pTest[cnt].name, inputArray);
+	            strcpy(promptMessage, "point => \n");
+	        }
+    	}
+    	while (point < 0) {
+    		point = getPointNumber(promptMessage, 0, 100);
+	        if (point >= 0) {
+	            pTest[cnt].point = point;
+	            strcpy(promptMessage, "name => \n");
+	        }
+    	}
         cnt += 1;
+    	result = -1;
+    	point = -1;
+    	printf("\n");
     }
 
     return 0;
+}
+
+/* 点数順ソート関数 */
+void sortPoint(ST_TEST *pTest){
+	int i;
+	int j;
+	int tmp;
+	char array[NAME_LEN];
+	
+	
+	for (i = 0; i < STUDENT_SIZE - 1; i++) {
+		for (j = i+1; j < STUDENT_SIZE; j++) {
+			if (pTest[j].point > pTest[i].point){
+				tmp = pTest[i].point;
+				strcpy(array, pTest[i].name);
+				pTest[i].point = pTest[j].point;
+				strcpy(pTest[i].name, pTest[j].name);
+				pTest[j].point = tmp;
+				strcpy(pTest[j].name, array);
+			}
+		}
+	}
+}
+
+/* 名前順ソート関数 */
+void sortName(ST_TEST *pTest){
+	int i;
+	int j;
+	int tmp;
+	char array[NAME_LEN];
+	
+	
+	for (i = 0; i < STUDENT_SIZE - 1; i++) {
+		for (j = i+1; j < STUDENT_SIZE; j++) {
+			if (strcmp(pTest[j].name, pTest[i].name) < 0){
+				tmp = pTest[i].point;
+				strcpy(array, pTest[i].name);
+				pTest[i].point = pTest[j].point;
+				strcpy(pTest[i].name, pTest[j].name);
+				pTest[j].point = tmp;
+				strcpy(pTest[j].name, array);
+			}
+		}
+	}
+}
+
+/* データ出力関数 */
+void printData(ST_TEST *pTest){
+	int i;
+	
+	printf("\nname\tpoint\n");
+    for(i = 0; i < 22; i++){
+        printf("-");
+    }
+	printf("\n");
+    for (i = 0; i < STUDENT_SIZE; i++) {
+        printf("%s\t%d\n", pTest[i].name, pTest[i].point);
+    }
+	printf("\n");
 }
