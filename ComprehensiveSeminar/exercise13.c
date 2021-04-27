3a8467c46152aad176155923b15df1364904721b
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-04-27 21:57:56 +0900

    exercise13.c (main関数)：まだ途中

--- ComprehensiveSeminar/exercise13.c
+++ ComprehensiveSeminar/exercise13.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "getFunc.h"
 
 #define NAME_LEN (20 + 2) /* 名前 20+\n+\0 */
@@ -13,75 +14,165 @@ struct ST_TEST {
 	ST_TEST *pNEXT;		/* 次へのポインタ */
 };
 
-int inputData(ST_TEST *pTest);
-void printData(ST_TEST *pTest);
+int inputNameData(ST_TEST *pTest);
+int inputPointData(ST_TEST *pTest);
+void printData(int data, ST_TEST* list);
+void DeleteData(int data, ST_TEST* list, char *buffer);
+ST_TEST* list_add(ST_TEST* list, char name[], int point);
 
 
 
 int main(void){
-	int result;
+	char promptMessage[] = "<<メニュー選択>>\n1:追加 2:削除 0:終了 => \n";
+    char inputArray[100];
+    int result = -1;
+	int point = -1;
     int i;
-    ST_TEST students[STUDENT_SIZE];
+	ST_TEST* list = NULL;
 	int type = 1;
+	int data = 5;
+	
+	
 	
 	while(1){
-		printf("<<メニュー選択>>\n1:追加 2:削除 0:終了 => ");
-		scanf("%d", &type);
+		//printf("<<メニュー選択>>\n1:追加 2:削除 0:終了 => ");
+		type = getNumber(promptMessage, 0, 2);
+		strcpy(promptMessage, "名前 => \n\n");
 		if (type == 0) {
 			break;
 		} else if (type == 1) {
-	    	result = inputData(&students[0]);
-			printf("%d", result);
-			printData(students);
+			//while (result < 0) {
+			result = getString(promptMessage, inputArray, 100);
+			strcpy(promptMessage, "点数 => \n");
+		    //}
+			//while (point < 0) {
+		    point = getNumber(promptMessage, 0, 100);
+			strcpy(promptMessage, "<<メニュー選択>>\n1:追加 2:削除 0:終了 => \n");
+			//}
+			if ((result >= 0) && (point >= 0)) {
+				list = list_add(list,  inputArray, point);
+				data -= 1;
+			}
 		} else {
-			// 削除
+			strcpy(promptMessage, "名前 => \n");
+			result = getString(promptMessage, inputArray, 100);
+			DeleteData(data, list, inputArray);
+			printf("削除");
+			strcpy(promptMessage, "<<メニュー選択>>\n1:追加 2:削除 0:終了 => \n");
 		}
+		printData(data, list);
 	}
     return 0;
 }
 
 
 /* データ入力関数 */
-int inputData(ST_TEST *pTest){
+int inputNameData(ST_TEST *pTest){
     char promptMessage[] = "名前 => ";
     char inputArray[100];
     int result = -1;
-    int point = -1;
 
     while (result < 0) {
 		result = getString(promptMessage, inputArray, 100);
     	printf("%d", result);
     	if (result >= 0) {
     		strcpy(pTest->name, inputArray);
-    		strcpy(promptMessage, "点数 => ");
     	}
     }
+	
+    return 0;
+}
+
+
+/* データ入力関数 */
+int inputPointData(ST_TEST *pTest){
+    char promptMessage[] = "点数 => ";
+    char inputArray[100];
+    int point = -1;
+
 	while (point < 0) {
     	point = getNumber(promptMessage, 0, 100);
 		printf("%d", point);
 		if (point >= 0) {
 			pTest->point = point;
-			strcpy(promptMessage, "名前 => ");
 		}
 	}
 	printf("\n");
-	pTest->pNEXT = pTest+1;
 
     return 0;
 }
 
 
 /* データ出力関数 */
-void printData(ST_TEST *pTest){
+void printData(int data, ST_TEST* list)
+{
+	printf("\n 名前\t点数\n------------\n");
+	while (1)
+    {
+        printf("%s\t%d\n", list->name, list->point);
+        if (list->pNEXT != NULL) {
+            list = list->pNEXT;
+        } else {
+        	printf("空きデータ数 %d\n\n", data);
+            break;
+        }
+    }
+}
+
+
+/* リストに要素を追加する */
+ST_TEST* list_add(ST_TEST* list, char name[], int point) {
+    /* 新しい要素を作成 */
+    ST_TEST* node = (ST_TEST*)malloc(sizeof(ST_TEST));
+	strcpy(node->name, name);
+    node->point = point;
+
+    /* 次の要素がないことを示すために NULL を代入 */
+    node->pNEXT = NULL;
+
+    if (list == NULL) {
+        /* リストが空の場合は特別扱いする */
+        return node;
+    }
+    else {
+        /* リストの末尾の要素を探す */
+        ST_TEST* p = list;
+        while (p->pNEXT != NULL) {
+            p = p->pNEXT;
+        }
+        /* ここで変数 p は末尾の要素をさす */
+        p->pNEXT = node;
+        return list;
+    }
+}
+
+
+/* データ削除関数 */
+void DeleteData(int data, ST_TEST* list, char *buffer)
+{
 	int i;
+	int cnt = 0;
+	int num;
+	ST_TEST* tmp[5];
 	
-	printf("\nname\tpoint\n");
-    for(i = 0; i < 22; i++){
-        printf("-");
+	while (1)
+    {
+		tmp[cnt] = list;
+    	if (*list->name == *buffer) {
+			num = cnt;
+			// free(list);
+    		// break;
+    	}
+        if (list->pNEXT != NULL) {
+			cnt += 1;
+            list = list->pNEXT;
+        } else {
+            break;
+        }
     }
-	printf("\n");
-    for (i = 0; i < STUDENT_SIZE; i++) {
-        printf("%s\t%d\t%d\t%d\n", pTest[i].name, pTest[i].point, pTest[i].pNEXT, pTest[i]);
-    }
-	printf("\n");
+	*tmp[num-1]->pNEXT = *tmp[num+1];
+	free(tmp[num]);
+	for (i = 0; i <= cnt; i++) {
+		printf("%d:%p->%s\n", i, tmp[i], tmp[i]->name);
+	}
 }
