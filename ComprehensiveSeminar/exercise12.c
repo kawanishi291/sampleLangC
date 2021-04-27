826b1a1762c2b904c40069cc392b29b8b13a3f83
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-04-27 20:26:51 +0900

    exercise12.c (main関数)

--- ComprehensiveSeminar/exercise12.c
+++ ComprehensiveSeminar/exercise12.c
@@ -3,19 +3,19 @@
 #include <string.h>
 #include "getFunc.h"
 
-#define ARRAY_AIZE (16)
-
-void printArray(int *p);
+void printInfo(void);
 
 
 int main(int argc, char *argv[]){
-	char promptString[] = "文字を入力してください(98文字以内)\ninput => ";
+	char promptString[] = "ファイル名を入力してください\ninput => \n";
     char inputArray[100];
+	int StringArray[100];
     FILE *fp;
+	FILE *tmp;
     int c;
 	int i;
 	int cnt = 0;
-	int array[ARRAY_AIZE + 2];
+	int add = 0;
 	
 	if (argc == 2) {
         strcpy(inputArray, argv[1]);
@@ -24,42 +24,84 @@ int main(int argc, char *argv[]){
     }
 	
 	fp = fopen(inputArray, "r");
+	tmp = fp;
     if (fp == NULL){
         printf("error\n");
         exit(1);
     }
-#if 1
+	printf("\n** 追加総合演習２開始 **\n\n");
+	printInfo();
     while ((c = fgetc(fp)) != EOF) {
+    	if(cnt == 0) {
+    		printf("%08d", add);
+    		printf("|");
+    		add += 10;
+    	}
+    	printf("%2X ", c);
+    	cnt += 1;
 	    if (isprint(c) != 0){
-			printf("%X ", c);
-	    	array[cnt] = c;
-	    	cnt += 1;
+	    	ungetc('c', stdin);
+	    	StringArray[cnt] = c;
 	    } else {
-	    	array[cnt] = (int)'\n';
-	    	//ungetc('.', fp);
+	    	ungetc('.', stdin);
+	    	StringArray[cnt] = (int)'.';
 	    }
     	if (cnt == 16) {
-    		printf("\t");
-    		printArray(array);
-			printf("\n");
+    		printf("|");
+    		for (i = 0; i <= cnt; i++) {
+    			printf("%c", StringArray[i]);
+    		}
     		cnt = 0;
-		}
+    		printf("\n");
+    	}
     }
-#endif
+	if (cnt < 16) {
+		for (i = 0; i < (16 - cnt); i++) {
+    		printf("   ");
+		}
+		printf("|");
+    	for (i = 0; i <= cnt; i++) {
+    		printf("%c", StringArray[i]);
+    	}
+    	cnt = 0;
+    	printf("\n");
+    }
+	printf("\n** 追加総合演習２終了 **\n");
+	
     fclose(fp);
 
     return 0;
 }
 
-
-void printArray(int *p){
+void printInfo(void)
+{
 	int i;
 	
-	for (i = 0; i < ARRAY_AIZE; i++) {
-		if (isprint(p[i]) != 0) {
-    		printf("%c", p[i]);
-		} else {
-			printf(".");
-		}
-    }
+	printf("アドレス|データの16進数表示                              |文字表示\n");
+	for (i = 0; i < 8; i++){
+		printf(" ");
+	}
+	printf("|");
+	for (i = 0; i < 16; i++){
+		printf("%2X ", i);
+	}
+	printf("| ");
+	for (i = 0; i < 16; i++){
+		printf("%X", i);
+	}
+	printf("\n");
+	
+	for (i = 0; i < 8; i++){
+		printf("-");
+	}
+	printf("+");
+	for (i = 0; i < 16; i++){
+		printf("---");
+	}
+	printf("+-");
+	for (i = 0; i < 16; i++){
+		printf("-");
+	}
+	printf("\n");
+	
 }
