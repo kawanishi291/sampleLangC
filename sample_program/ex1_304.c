e6b9c3cd9356f222d47748c075b260faa4fa59eb
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-04-30 20:02:40 +0900

    文字列の編集出力

---
+++ sample_program/ex1_304.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(void){
+	/* 文字列の編集出力 */
+	char str1[] = "毎朝";
+	char str2[] = "朝食";
+	int hour = 8;
+	
+	
+	
+	printf("私は%s、%d時に%sを食べます\n", str1, hour, str2);
+	
+	return 0;
+}
