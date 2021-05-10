ba4fd976cebde417be43c75e4025b4f593a7bfe6
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-05-10 22:52:09 +0900

    課題2-3．（四則演算）

---
+++ kadai/kadai02_03.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main(){
+    int x;
+    int y;
+
+    printf("x => \n");
+    scanf("%d", &x);
+    printf("y => \n");
+    scanf("%d", &y);
+    printf("x + y = %d\n", x + y);
+    printf("x - y = %d\n", x - y);
+    printf("x * y = %d\n", x * y);
+    printf("x / y = %d\n", x / y);
+
+}
