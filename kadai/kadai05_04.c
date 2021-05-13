7b6c336f894c38e4ba89bf75514b34a9580559a9
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-05-13 22:51:57 +0900

    課題5-4．（while文による反復，デクリメント）

---
+++ kadai/kadai05_04.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+  
+int main(){
+    int num;
+
+    printf("数値を入力してください => \n");
+    scanf("%d", &num);
+    while(num > 0) {
+        printf("%d, ", num);
+        num --;
+    }
+    printf("0");
+    
+
+    return 0;
+}
