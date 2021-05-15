7b062f435aa0a0c7a1242f2388ce7c4866c2bb40
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-05-15 13:27:52 +0900

    No. 80 互いに素

---
+++ knock_100/knock_080.c
@@ -0,0 +1,46 @@
+/*
+2つの正の整数値を入力させ、互いに素であるか判定するプログラムを作成せよ。
+なお、2つの正の整数が互いに素とは、1以外に共通公約数を持たない関係のことである。
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int PrimeNumber(int num);
+
+int main(void) {
+    int num1;
+    int num2;
+    int flag1;
+    int flag2;
+
+    printf("2つの値をスペースで区切って入力：\n");
+    scanf("%d %d", &num1, &num2);
+    flag1 = PrimeNumber(num1);
+    flag2 = PrimeNumber(num2);
+    if (flag1 == 1 && flag2 == 1) {
+        printf("互いに素\n");
+    } else {
+        printf("互いに素でない\n");
+    }
+
+    return 0;
+}
+
+int PrimeNumber(int num)
+{
+    int i;
+    int cnt = 0;
+
+    for (i = 1; i <= num; i++) {
+        if (num % i == 0) {
+            cnt ++;
+        }
+    }
+    if (cnt == 2) {
+        return 1;
+    } else {
+        return 0;
+    }
+
+}
