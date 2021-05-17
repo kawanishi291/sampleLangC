df028d75ebd9de62db5ac8774299b5b1e60280ba
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-05-17 21:34:49 +0900

    No. 53 素因数分解

--- knock_100/knock_053.c
+++ knock_100/knock_053.c
@@ -1,8 +1,29 @@
 /*
 自然数の入力値を素因数分解して結果を表示するプログラムを作成せよ。
+まず2で割り切れる間は割っていき、2で割り切れなくなったら3で、と、割る数を1ずつ大きくしながら繰り返す。
 */
 # include <stdio.h>
 
+int main(){
+    int num;
+    int prime_num = 2;
+
+
+    printf("input year:\n");
+    scanf("%d", &num);
+    while (num != 1) {
+        if (num % prime_num == 0) {
+            printf("%d ", prime_num);
+            num /= prime_num;
+        } else {
+            prime_num ++;
+        }
+    }
+
+    return 0;
+}
+
+/*
 int main(){
     int num;
     int prime_numbers[11] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
@@ -34,3 +55,4 @@ int main(){
 
     return 0;
 }
+*/
