0e8b80a36b777a6c19a88501ce70ce6c0f24d89e
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-05-18 22:58:15 +0900

    No. 92 世界の人

--- knock_100/knock_092.c
+++ knock_100/knock_092.c
@@ -9,6 +9,10 @@ int main(){
     for (i = 1; i <= 50; i++) {
         if (i % 3 == 0) {
             printf("aho %d\n", i);
+        } else if (i % 10 == 3) {
+            printf("aho %d\n", i);
+        } else if (30 <= i && i <= 39) {
+            printf("aho %d\n", i);
         } else {
             printf("%d\n", i);
         }
