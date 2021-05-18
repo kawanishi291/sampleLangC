4e57161930cc9b28ecf4b3fb70f7231e591caf04
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-05-18 22:46:55 +0900

    No. 92 世界の人

---
+++ knock_100/knock_092.c
@@ -0,0 +1,19 @@
+/*
+1から50まで順に表示していくが、3の倍数と3のつく数字のときは頭に"aho"と付けて表示するプログラムを作成せよ。
+*/
+# include <stdio.h>
+
+int main(){
+    int i;
+    
+    for (i = 1; i <= 50; i++) {
+        if (i % 3 == 0) {
+            printf("aho %d\n", i);
+        } else {
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
+}
+
