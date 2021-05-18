5e15633a368168c7d550683fc4cedd5e3f1ed06e
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-05-18 23:10:14 +0900

    No. 93 宇宙の人

--- knock_100/knock_093.c
+++ knock_100/knock_093.c
@@ -4,10 +4,13 @@
 */
 # include <stdio.h>
 
+int checkNumberIn3(int num);
+
 int main(){
     int i;
     int start;
     int end;
+    int flag;
     
     printf("start:\n");
     scanf("%d", &start);
@@ -17,10 +20,30 @@ int main(){
         if (i % 3 == 0) {
             printf("aho %d\n", i);
         } else {
-            printf("%d\n", i);
+            flag = checkNumberIn3(i);
+            if (flag == 1) {
+                printf("aho %d\n", i);
+            } else {
+                printf("%d\n", i);
+            }
         }
     }
 
     return 0;
 }
 
+int checkNumberIn3(int num)
+{
+    int flag = 0;
+
+    while (num > 0) {
+        if (num % 10 == 3) {
+            flag = 1;
+            break;
+        }
+        num -= num % 10;
+        num /= 10;
+    }
+
+    return flag;
+}
