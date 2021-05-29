811e36912e024f779428130487d55c99215e3309
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-05-29 21:01:11 +0900

    1畳・1平米・1坪の早見表

--- temporary/sample05.c
+++ temporary/sample05.c
@@ -4,22 +4,27 @@
 
 int main(){
     int flag;
-    int m2;
+    float m2;
     int z;
-    int h;
+    int t;
 
-    printf("0:畳, 1:㎡, 2:坪");
+    printf("0:畳, 1:㎡, 2:坪\n");
     scanf("%d", &flag);
     switch(flag) {
         case 0:
             printf("畳：\n");
             scanf("%d", &z);
-            printf("%d畳, %.2f㎡, 約%d坪", z, z * 1.62, z / 2);
+            printf("%d畳, %.2f㎡, 約%.1f坪\n", z, z * 1.62, (float)z / 2);
             break;
         case 1:
             printf("㎡：\n");
-            scanf("%d", &m2);
-            printf("%d畳, %.2f㎡, 約%d坪", m2 / 1.62, m2, (m2 / 1.62) / 2);
+            scanf("%f", &m2);
+            printf("%.2f畳, %.2f㎡, 約%.2f坪\n", m2 / 1.62, m2, (m2 / 1.62) / 2);
+            break;
+        case 2:
+            printf("坪：\n");
+            scanf("%d", &t);
+            printf("%d畳, %.2f㎡, 約%d坪\n", t * 2, t * 2 * 1.62, t);
             break;
     }
 
