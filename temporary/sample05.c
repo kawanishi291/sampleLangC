0af500e6386cd60a737d9739f1da30b8bdfbb93e
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-05-29 20:07:58 +0900

    変更

--- temporary/sample05.c
+++ temporary/sample05.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 
+
+
 int main(){
+    int flag;
+    int m2;
     int z;
     int h;
 
-    printf("畳：\n");
-    scanf("%d", &z);
-    printf("%d畳, %.2f㎡, 約%d坪", z, z * 1.62, z / 2);
+    printf("0:畳, 1:㎡, 2:坪");
+    scanf("%d", &flag);
+    switch(flag) {
+        case 0:
+            printf("畳：\n");
+            scanf("%d", &z);
+            printf("%d畳, %.2f㎡, 約%d坪", z, z * 1.62, z / 2);
+            break;
+        case 1:
+            printf("㎡：\n");
+            scanf("%d", &m2);
+            printf("%d畳, %.2f㎡, 約%d坪", m2 / 1.62, m2, (m2 / 1.62) / 2);
+            break;
+    }
 
 }
